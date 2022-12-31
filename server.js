77523129ef3e5141872c6639ce4d0304a765e911
Sangrail <jaycoleman@gmail.com>
2022-12-31 23:56:01 +0000

    Update msg!

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { hostname } = require('os');
 const http = require('http');
-const message = 'Hello cloud ci/cd!\n';
+const message = 'Hello cloud ci/cd!!\n';
 const port = 8080;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
